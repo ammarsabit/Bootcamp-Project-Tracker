f22e4386fc577d976aee91b8604fb9d629128d25
Ammar Sabit <ammarsabit39@gmail.com>
2025-08-21 01:06:04 +0300

    Installed cors to connect to the local frontend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import helmet from "helmet";
 import router from "./routes/projects.js";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(helmet());
 
 app.use("/api/projects", router);
